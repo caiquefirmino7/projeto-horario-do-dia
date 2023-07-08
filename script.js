b39b5e900de8a467871a97b041425c48181ea45b
Caíque Firmino <caiquefes7@gmail.com>
2023-07-08 14:53:01 -0300

    Mudanças funcionais

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    let minutos = data.getMinutes()
+    let segundos = data.getSeconds()
+
    
     
-    msg.innerHTML= `<strong>Agora são ${hora} horas</strong> <br> `
+    msg.innerHTML= `<strong>Agora são ${hora}:${minutos}:${segundos} </strong> <br> `
     
     if (hora>=0 && hora <12){
         //Bom dia
-        img.src= './imagens/manha.jpg'
+        img.src= 'imagens/manha.jpg'
         document.body.style.background = '#e2cd9f'
         msg.innerText+= `Bom dia!`
     } else if (hora >= 12 && hora <18) {
         //boa tarde
-        img.src= '.imagens/tarde.jpg'
+        img.src= 'imagens/tarde.jpg'
         document.body.style.background = '#FF8C00'
         msg.innerText+= `Boa tarde!`
     } else{
         //boa noite
-        img.src= 'noite.jpg'
+        img.src= 'imagens/noite.jpg'
         document.body.style.background = '#191970'
         msg.innerText+= `Boa noite!`
     }
 }
+
+setInterval(carregar, 1000);
